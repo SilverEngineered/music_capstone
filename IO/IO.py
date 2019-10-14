0efa70593b13abda0185a7f84795d7052cd952b3
daniel.silver <daniel@silverengineered.com>
2019-10-14 14:53:10 -0400

    preliminary listen

--- IO/IO.py
+++ IO/IO.py
@@ -1,14 +1,18 @@
 import board
 from neopixel import NeoPixel
+import mido
+import rtmidi
 
 class IO(object):
 	def __init__(self, num_keys):
 		self.num_keys = num_keys
 		self.pixels = NeoPixel(board.D18, self.num_keys)
-
+		self.inputs = mido.get_input_names()
 
 	def listen(self):
-		print("listening")
+		with mido.open_input(self.inputs[0]) as port:
+			for msg in port:
+				print(msg)
 
 	def light(self,key):
 		print("lighting")
