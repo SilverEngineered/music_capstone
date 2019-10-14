4196b6686eeda1dfe5462e3412b2cd94d93088b5
daniel.silver <daniel@silverengineered.com>
2019-10-14 14:03:41 -0400

    added main template

---
+++ IO/scripts/IO.py
@@ -0,0 +1,11 @@
+
+class IO(object):
+
+    def __init__(self, num_keys):
+        self.num_keys = num_keys
+
+    def listen_for_next(self):
+        pass
+
+    def light_up_next(self):
+        pass
