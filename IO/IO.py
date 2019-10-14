e8f95cb71342fc5ce49f86cfc514ea022a570475
daniel.silver <daniel@silverengineered.com>
2019-10-14 14:42:00 -0400

    python3 version changes

--- IO/IO.py
+++ IO/IO.py
@@ -1,12 +1,15 @@
-from Strip import Strip
+import board
+from neopixel import NeoPixel
 
 class IO(object):
 	def __init__(self, num_keys):
 		self.num_keys = num_keys
-		s = Strip(self.num_keys)
+		self.pixels = NeoPixel(board.D18, self.keys)
+
 
 	def listen(self):
 		print("listening")
 
-	def light(self):
+	def light(self,key):
 		print("lighting")
+		self.pixels[key] = (255, 255, 255)
