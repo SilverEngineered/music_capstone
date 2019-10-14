e257c399a1c36548e7b8b7292061ffaaa384e285
daniel.silver <daniel@silverengineered.com>
2019-10-14 14:14:52 -0400

    fixed tabs and spaces

--- IO/IO.py
+++ IO/IO.py
@@ -1,7 +1,10 @@
+import Strip
+
 class IO(object):
-    def __init__(self, num_keys):
+	def __init__(self, num_keys):
 		self.num_keys = num_keys
-		
+		s = Strip(self.num_keys)
+
 	def listen(self):
 		pass
 
