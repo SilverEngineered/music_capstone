42e940fe3c30e5c3a8fd09b059526851349bf05e
daniel.silver <daniel@silverengineered.com>
2019-10-14 15:18:58 -0400

    added tuple

--- IO/IO.py
+++ IO/IO.py
@@ -10,17 +10,17 @@ class IO(object):
 		self.num_keys = num_keys
 		self.pixels = NeoPixel(board.D18, self.num_keys)
 		self.inputs = mido.get_input_names()
-		self.queue = [[]]
+		self.queue = [()]
 
 	def listen(self, duration):
 		msg_list = []
-		start = time.time()
 		with mido.open_input(self.inputs[0]) as port:
+			start = time.time()
 			for msg in port:
 				if time.time() - start > duration:
 					return msg_list
 				else:
-					msg_list.append(msg)
+					msg_list.append((msg, start - time.time()))
 
 	def light(self, key, color):
 		print("lighting")
