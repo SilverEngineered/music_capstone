83156486f8698a323fac1c7b674648784d28c0e6
daniel.silver <daniel@silverengineered.com>
2019-10-14 14:43:13 -0400

    fixed small bug

--- IO/IO.py
+++ IO/IO.py
@@ -4,7 +4,7 @@ from neopixel import NeoPixel
 class IO(object):
 	def __init__(self, num_keys):
 		self.num_keys = num_keys
-		self.pixels = NeoPixel(board.D18, self.keys)
+		self.pixels = NeoPixel(board.D18, self.num_keys)
 
 
 	def listen(self):
