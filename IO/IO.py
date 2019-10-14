07db69168b4ed16be0d8096f101db48873400693
daniel.silver <daniel@silverengineered.com>
2019-10-14 13:39:36 -0400

    added template

---
+++ IO/IO.py
@@ -0,0 +1,10 @@
+class IO(object):
+    def __init__(self, args):
+        self.grace = args.grace
+        self.notes = args.notes
+
+    def listen_for_next(self):
+        pass
+
+    def light_up_next(self):
+        pass
