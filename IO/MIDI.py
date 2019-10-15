181fdb33fb359e5106b70d5e354ee944781fc120
daniel.silver <daniel@silverengineered.com>
2019-10-14 22:31:18 -0400

    removed functions that are now in IO.py

--- IO/MIDI.py
+++
@@ -1,9 +0,0 @@
-import mido
-import rtmidi
-
-if __name__ == '__main__': 
-
-    inputs = mido.get_input_names()
-    with mido.open_input(inputs[0]) as port:
-        for msg in port:
-            print (msg)
