a9a2dab2042d7096831758beb18eabd0511d7343
daniel.silver <daniel@silverengineered.com>
2019-10-14 22:58:43 -0400

    added IO into main

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import argparse
+from IO.IO import IO
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_keys', default=61)
+args = parser.parse_args()
 if __name__ == "__main__":
-    pass
+    io = IO(args.num_keys)
